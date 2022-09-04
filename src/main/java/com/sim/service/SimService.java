4869b7eb12511758a36c17366f4fee1a26f63a7b
Vijeyakumar R <vijeyakumar.26@gmail.com>
2022-09-04 17:18:12 +0530

    updated service

--- src/main/java/com/sim/service/SimService.java
+++ src/main/java/com/sim/service/SimService.java
@@ -1,11 +1,13 @@
 package com.sim.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sim.entity.Sim;
+import com.sim.exception.SimNotFoundException;
 import com.sim.repository.SimRepository;
 
 @Service
@@ -18,17 +20,22 @@ public class SimService {
 		return simRepository.findAll();
 	}
 	
-	
 	public void addSim(Sim sim) {		
 		simRepository.save(sim);
 	}
 
-	public void updateSimInfo(Sim sim) {
-		simRepository.save(sim);
+	public void updateSimInfo(Long simNo, boolean kyc, String telecom) {
+		Optional<Sim> simFound = simRepository.findById(simNo);
+		if(simFound.isPresent()) {
+			Sim sim = simFound.get();
+			sim.setKyc(kyc);
+			sim.setTelecomProvider(telecom);
+			simRepository.save(sim);
+		}
 	}
 
-	public void deleteSim(String placeName) {
-		simRepository.delete(null);
+	public void deleteSim(Long simNo) {	
+		simRepository.deleteById(simNo);		
 	}
 }
 
