923d9a54fdcfe908bade046ec08970c475c2128b
Vijeyakumar R <vijeyakumar.26@gmail.com>
2022-09-05 23:44:45 +0530

    updated exception

--- src/main/java/com/sim/exception/SimNotFoundException.java
+++ src/main/java/com/sim/exception/SimNotFoundException.java
@@ -1,12 +1,24 @@
 package com.sim.exception;
 
-public class SimNotFoundException extends Exception {
-	/**
-	 * 
-	 */
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class SimNotFoundException extends RuntimeException {
+	
 	private static final long serialVersionUID = 1L;
 
 	public SimNotFoundException(String errorMessage) {
-        super(errorMessage);
-    }
+		super(errorMessage);
+	}
+	public SimNotFoundException() {
+		super();
+	}
+	public SimNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public SimNotFoundException(Throwable cause) {
+		super(cause);
+	}
 }
