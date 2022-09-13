8b8f25a69479cc57eeef76360cac29521cf987bc
Vijeyakumar R <vijeyakumar.26@gmail.com>
2022-09-13 22:16:10 +0530

    added basic security

--- src/main/java/com/sim/config/SecurityConfig.java
+++ src/main/java/com/sim/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +15,9 @@ public class SecurityConfig  {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
-            )
+            .authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
             .httpBasic(withDefaults())
-            .csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+            .csrf().disable();
         return http.build();
     }
 
